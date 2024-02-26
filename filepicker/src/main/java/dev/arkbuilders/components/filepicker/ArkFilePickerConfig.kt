4c142842da0cdfcedec1d3335edcbb490f9253d5
tuancoltech <tuancoltech@gmail.com>
2024-02-26 22:16:26 +0700

    Restructure ark-android repository: https://github.com/ARK-Builders/ark-android/issues/55

---
+++ filepicker/src/main/java/dev/arkbuilders/components/filepicker/ArkFilePickerConfig.kt
@@ -0,0 +1,30 @@
+package dev.arkbuilders.components.filepicker
+
+import androidx.annotation.PluralsRes
+import androidx.annotation.StringRes
+import androidx.annotation.StyleRes
+import dev.arkbuilders.components.filepicker.R
+import java.nio.file.Path
+
+class ArkFilePickerConfig(
+    @StringRes
+    val titleStringId: Int = R.string.ark_file_picker_pick_title,
+    @StringRes
+    val pickButtonStringId: Int = R.string.ark_file_picker_pick,
+    @StringRes
+    val cancelButtonStringId: Int = R.string.ark_file_picker_cancel,
+    @StringRes
+    val internalStorageStringId: Int = R.string.ark_file_picker_internal_storage,
+    @StringRes
+    val accessDeniedStringId: Int = R.string.ark_file_picker_access_denied,
+    @PluralsRes
+    val itemsPluralId: Int = R.plurals.ark_file_picker_items,
+    @StyleRes
+    val themeId: Int = com.google.android.material.R.style.Theme_MaterialComponents_Light_Dialog,
+
+    val showRoots: Boolean = false,
+    val rootsFirstPage: Boolean = false,
+    val initialPath: Path? = null,
+    val mode: ArkFilePickerMode = ArkFilePickerMode.FOLDER,
+    val pathPickedRequestKey: String? = null,
+)
